5940ce474c81c7baa63534557f71c4d6d505e2dd
yueenen <yueenen@yueenendeMacBook-Air.local>
2016-12-13 03:46:27 +0800

    waterfall

--- waterfall.js
+++ waterfall.js
@@ -2,3 +2,68 @@
  * Created by yueenen on 12/13/16 02:09.
  * 你不会的原因是想的太多，写得太少。
  */
+
+$.fn.waterfall = function () {
+    var pad = 20;
+
+    var totalWidth = $(this).width();
+
+    var items = $(this).children();
+
+    var itemWidth = items.width();
+
+    var cols = Math.floor(totalWidth/itemWidth);
+
+    var itemHeight;
+
+    var itemY = [];
+
+    items.each(function (index,value) {
+
+        itemHeight = $(value).height();
+        
+        //console.log("aa");
+
+        if(index < cols){
+            $(value).css({
+                top : 0,
+                left: pad + (pad +itemWidth)*index
+            });
+
+            itemY[index] = itemHeight;
+        }else {
+            var minCol = 0;
+            var minHeight = itemY[minCol];
+
+            for(var i = 0; i < cols; i ++){ //??????
+                if(minHeight > itemY[i]){
+                    minCol = i;
+                    minHeight = itemY[i];
+                }
+            }
+            $(value).css({
+
+                top: minHeight + pad,
+
+                left: pad + (pad + itemWidth)*minCol
+
+            });
+
+            itemY[minCol] = itemY[minCol]+pad + itemHeight;
+        }
+
+    });
+
+    var maxCol = 0;
+
+    var maxHeight = itemY[maxCol];
+
+    for(var i = 1; i < cols; i++){
+
+        if(maxHeight< itemY[i]){
+            maxCol = i;
+            maxHeight = itemY;
+        }
+    }
+    $(".items").height(maxHeight+pad);
+};
--- waterfall.php
+++ waterfall.php
@@ -1,3 +1,26 @@
 <?php
-    header('Content-Type:text/html;charset=utf-8');
+    header("Content-Type:application/json;charset=utf-8");
+
+    $data=file_get_contents("data.json");
+
+    $arr = json_decode($data);
+
+    $page = $_GET["page"];
+
+    $start = ($page - 1)*10;
+
+    $arr = array_slice($arr,$start,10);
+
+    $page++;
+
+    $result = array(
+        "items" => $arr,
+        "page"=>$page
+    );
+
+    $jsonStr = json_encode($result);
+
+    echo $jsonStr;
+
+    sleep (1);
 ?>
