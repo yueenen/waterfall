25c94b6a2d955bce464a6853158de059d2b2e3b0
yueenen <yueenen@yueenendeMacBook-Air.local>
2016-12-13 21:45:58 +0800

    new waterfall

--- waterfall.js
+++ waterfall.js
@@ -2,6 +2,21 @@
  * Created by yueenen on 12/13/16 02:09.
  * 你不会的原因是想的太多，写得太少。
  */
+/*
+ * 4.1 往jQuery的原型中添加一个瀑布流方法
+ * 4.2 定义好padding, 获取屏幕宽度
+ * 4.3 获取item的个数
+ * 4.4 获取items的宽度
+ * 4.5 定义好列数: 总宽度/item的宽度
+ * 4.6 声明item高度
+ * 4.7 创建数组存储制定列数的高度值
+ * 4.8 用each遍历每一个item(返回index和value),给每一个item设置定位
+ * 4.8.1 瀑布流第一行,就是通过计算列数范围内的值(小于总列数),给其设置top和left值
+ * 4.8.2 瀑布流其它行: 设置最小高度的列数是第0列, 遍历所有列数,找到最终高度最小的列(index)
+ * 4.8.3 设置 top: 最小高度 + padding
+ * 4.8.4 设置 left: padding + (padding + itemWidth) * index
+ * 4.9 tips 提示条: 遍历获取最大高度 maxHeight, 设置 items 的高度为 maxHeight + padding
+ * */
 
 $.fn.waterfall = function () {
     var pad = 20;
@@ -12,31 +27,31 @@ $.fn.waterfall = function () {
 
     var itemWidth = items.width();
 
-    var cols = Math.floor(totalWidth/itemWidth);
+    var cols = Math.floor(totalWidth / itemWidth);
 
     var itemHeight;
 
     var itemY = [];
 
-    items.each(function (index,value) {
+    items.each(function (index, value) {
 
         itemHeight = $(value).height();
-        
+
         //console.log("aa");
 
-        if(index < cols){
+        if (index < cols) {
             $(value).css({
-                top : 0,
-                left: pad + (pad +itemWidth)*index
+                top: 0,
+                left: pad + (pad + itemWidth) * index
             });
 
             itemY[index] = itemHeight;
-        }else {
+        } else {
             var minCol = 0;
             var minHeight = itemY[minCol];
 
-            for(var i = 0; i < cols; i ++){ //??????
-                if(minHeight > itemY[i]){
+            for (var i = 0; i < cols; i++) { //??????
+                if (minHeight > itemY[i]) {
                     minCol = i;
                     minHeight = itemY[i];
                 }
@@ -45,11 +60,11 @@ $.fn.waterfall = function () {
 
                 top: minHeight + pad,
 
-                left: pad + (pad + itemWidth)*minCol
+                left: pad + (pad + itemWidth) * minCol
 
             });
 
-            itemY[minCol] = itemY[minCol]+pad + itemHeight;
+            itemY[minCol] = itemY[minCol] + pad + itemHeight;
         }
 
     });
@@ -58,12 +73,12 @@ $.fn.waterfall = function () {
 
     var maxHeight = itemY[maxCol];
 
-    for(var i = 1; i < cols; i++){
+    for (var i = 1; i < cols; i++) {
 
-        if(maxHeight< itemY[i]){
+        if (maxHeight < itemY[i]) {
             maxCol = i;
             maxHeight = itemY;
         }
     }
-    $(".items").height(maxHeight+pad);
+    $(".items").height(maxHeight + pad);
 };
--- waterfall.php
+++ waterfall.php
@@ -1,6 +1,15 @@
 <?php
     header("Content-Type:application/json;charset=utf-8");
 
+/**
+3.1 读取 json 文件数据(字符串值)
+3.2 将 json 字符串转换为 php 对象  //{}转换为PHP对象, []转换为PHP数组
+3.3 获取html页面中传递的页码
+3.4 获取开始位置
+3.5 获取数组中从指定位置开始的指定数量的元素,返回一个数组
+3.6 将PHP数组转换成 json 字符串
+3.7 将数据输出到html页面中
+**/
     $data=file_get_contents("data.json");
 
     $arr = json_decode($data);
